3cb88ca597e9ae21d07b56fad7f7dc33a09b98d4
4simov <corentin.pauline@outlook.com>
2024-01-14 23:45:51 +0100

    gestion des sondes et ébauche des réquêtes coté site web

--- Back/Controller/sondeController.py
+++ Back/Controller/sondeController.py
@@ -6,13 +6,17 @@ sonde_route = Blueprint('sonde', __name__)
 class SondeController() :
     @sonde_route.get('/sonde')
     def get():
-        return 'Fonction GET all sonde'
+        sondes = Sonde.query.all()
+        return jsonify([s.to_json() for s in sondes])
+        #return jsonify({ 'Sonde' : [s.to_json() for s in sondes]})
 
     @sonde_route.get('/sonde/<idS>')
     def get_by_id(idS):
         s = Sonde.query.get(idS)
         if s :
-            return s.to_json()
+            rep = s.to_json()
+            print(rep)
+            return rep
         else :
             return 'Pas de sonde associées à cette id'
 
@@ -37,23 +41,9 @@ class SondeController() :
             return "Cette sonde n'existe pas"
         else :
             if request.is_json:
-                print(str(request.get_json("activation")))
-                s.activate = request.get_json("activation")
+                s.activate = request.json.get("activation")
             else :
                 return "il manque des paramètres dans le json"
-            db.session.commit()
-            return s.to_json()
-    
-    @sonde_route.put('/sonde/<idS>/close')
-    def close_sonde(idS) :
-        s = Sonde.query.get(idS)
-        if s is None:
-            return "Cette sonde n'existe pas"
-        else :
-            if request.is_json:
-                s.activate  = False
-                
-            else :
-                return "il manque des paramètres dans le json"
+            db.session.add(s)
             db.session.commit()
             return s.to_json()
--- Back/Models/sondeModel.py
+++ Back/Models/sondeModel.py
@@ -4,7 +4,7 @@ db = db
 
 class Sonde(db.Model):
     id = db.Column(db.String, primary_key=True, nullable = False)
-    activate = db.Column(db.Boolean)
+    activate = db.Column(db.Boolean, default = True, nullable = False)
     sonde = db.relationship('Releve', backref='sonde', lazy = True)
 
     def to_json(self):
